2d3f8043724806678194fb8e10fb822536a064b1
velveet <velveet@pm.me>
2020-08-28 21:28:04 -0500

    react-typical imported. Steps added to typical (1000). App.css modified.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <h3>Hi, I'm velveet</h3>
+        <p>I am a {' '}
+          <Typical 
+            loop={Infinity}
+            wrapper='b'
+            steps={[
+              'FullStack Developer',
+              1000,
+              'Mechatronic Engineer',
+              1000,
+              'Writer',
+              1000,
+              'Dreamer',
+              1000,
+              'Data Scientist',
+              1000
+            ]}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
